4b3ae47978dd798bcf1e631e31c7a7e61f43c759
Silas-assis <silaslima-19@hotmail.com>
2021-09-28 16:49:11 -0300

    Page Perfil Criado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,11 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },  {
+    path: 'perfil',
+    loadChildren: () => import('./perfil/perfil.module').then( m => m.PerfilPageModule)
   },
+
 ];
 
 @NgModule({
